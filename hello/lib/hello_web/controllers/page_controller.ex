8ad1af2d46dca915dba994e6e5e30876d94db39a
Songhyeon Jun <shjeon0730@gmail.com>
2025-01-01 20:23:38 -0600

    phoenix first test

---
+++ hello/lib/hello_web/controllers/page_controller.ex
@@ -0,0 +1,15 @@
+defmodule HelloWeb.PageController do
+  use HelloWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+
+  def hello(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :hello, layout: false)
+  end
+end
